6427cbf36ce587f17552d19273290b07d269b286
ai7hoo <renqunai7hoo@gmail.com>
2015-05-05 12:19:24 +0800

    update bind-live-delegate.html

--- 0505/custom.js
+++ 0505/custom.js
@@ -1,17 +1,7 @@
-//text/javascript
-$(document).ready(function() {
-//	$(".btn").live('click',function() {
-//		alert($(this).html());
-//	});
-//	在1.7+版本中live()已经变更为on()
-//	在1.4+版本中用delegate()
-//	在1.3+版本中用live()
-
-
-	var myurl = "http://www.baidu.com/about.html";
-	var en_myurl = encodeURIComponent(myurl);
-	var un_en_myurl = decodeURIComponent(en_myurl);
-	alert(un_en_myurl);
-
-
+$(function () {
+	$('.btn').bind('click', function () {
+		$.each( $('.box li') ,function(i,n) {
+			$('.arrbox').append(i + '===' + $('.box li').eq(i).text() + '<br \/>');
+		});
+	});
 });
