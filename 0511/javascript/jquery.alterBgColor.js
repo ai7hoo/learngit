1cb20f674dc0b9338201f6286210e578f5645468
ai7hoo <renqunai7hoo@gmail.com>
2015-05-11 15:01:54 +0800

    update 0511

--- 0511/javascript/jquery.alterBgColor.js
+++ 0511/javascript/jquery.alterBgColor.js
@@ -2,27 +2,37 @@
 //$("selected").alterBgColor(options);
 //参数为对象,{odd:"odd",even:"even",selected:"selected"}
 //带默认参数
-;(function($){
+; (function ($) {
 	$.fn.extend({
-		"alterBgColor": function( options ){
-			$.extend({
-				odd: "tb-odd",
-				even: "tb-even",
-				selected: "selected"
-			}, options);
-			
-			//
-			$("tbody>tr:odd", this).addClass("tb-odd");
-			$("tbody>tr:even", this).addClass("tb-even");
-			
-			$("tbody>tr", this).click(function(){
-				$(this).siblings('tr').removeClass("tb-selected");
-				$(this).toggleClass("tb-selected");
+		"alterBgColor": function (options) {
+			return this.each(function () {
+				$.extend({
+					odd: "tb-odd",
+					even: "tb-even",
+					selected: "td-selected",
+					hover: "td-hover"
+				}, options);
+				$("tbody>tr:odd", this).addClass("tb-odd");
+				$("tbody>tr:even", this).addClass("tb-even");
+				$("tbody>tr", this).click(function () {
+					$(this).siblings('tr').removeClass("tb-selected");
+					$(this).toggleClass("tb-selected");
+					if ($(this).hasClass("tb-selected")) {
+						$(this).removeClass("tb-hover");
+					}
+				});
+				$("tbody>tr", this).mouseover(function () {
+					if (!$(this).hasClass("tb-selected")) {
+						$(this).addClass("tb-hover");
+					}
+				}).mouseout(function () {
+					$(this).removeClass("tb-hover");
+				});
+				return this;
 			});
-			
-			return this;
-			
-			
+
+
+
 		}
 	});
 })(jQuery);
