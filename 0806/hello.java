d5190820f0d3cf2a9012206a05f3ba2c8f32c136
ai7hoo <renqunai7hoo@gmail.com>
2015-08-11 21:55:14 +0800

    add 0806

--- 0806/hello.java
+++ 0806/hello.java
@@ -6,7 +6,7 @@
 */
 
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("hello java");
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
