ebd9fe7d91ca577416e18cd90e7b184a19b1941d
ai7hoo <renqunai7hoo@gmail.com>
2015-09-19 23:04:53 +0800

    add 0917

--- 0917/js/main.js
+++ 0917/js/main.js
@@ -2,36 +2,99 @@ $(function(){
 	var box = new Vue({
 		el: "#box",
 		data: {
-			id:2,
+			cat:2,
 			message:mm(),
 			content:'content',
-			// todos:[
-			// 	{
-			// 		on:false,
-			// 		text:'text'
-			// 	},
-			// 	{
-			// 		on:false,
-			// 		text:'haha'
-			// 	}
-			// ],
+			todos:[
+				{
+					id:'10',
+					text:'text'
+				},
+				{
+					id:'32',
+					text:'haha'
+				}
+			],
+			list:[
+				{
+					id:'1',
+					title:'t1',
+					href:'http://www.baidu.com'
+				},{
+					id:'2',
+					title:'t2',
+					href:'http://www.qq.com'
+				}
+			]
+		},
+		methods:{
+			clickEvent:function(str,e){
+				alert(str);
+				e.stopPropagation();
+			}
 		}
 	});
-	box.$data.$todos='0';
+
+	var form = new Vue({
+			el:'#form',
+			data:{
+				msg:'hi',
+				checked:true,
+				selected:'one',
+				multiSelect:'two'
+			}
+		});
 	
 	$.ajax({
 		url:'http://localhost/testdata.json',
 		type:'get',
 		timeout:15000,
-		success:function(data){
-			for(var i=0;i<data.data.length;i++){
-				console.log(i);
+		success:function(dt){
+			for(var i=0;i<dt.data.length;i++){
+				//box.$data.todos.$set(i,{id:dt.data[i].ID,text:dt.data[i].post_title});
 			}
 		},
 		error:function(err){
 			alert(err);
 		}
 	});
+
+	var computer = new Vue({
+		el:"#compupter",
+		data:{
+			firstName:'Ren',
+			lastName:'Qun'
+		},
+		computed:{
+			fullName:function(){
+				return this.firstName + ' ' + this.lastName;
+			}
+		},
+		methods:{
+			reBack:function(){
+				alert('reBack');
+			}
+		}
+	});
+
+	Vue.directive('mydefine',{
+		bind:function(){
+			this.el.style.color='#f00';
+			this.el.style.backgroundColor='#000';
+		},
+		update:function(value){
+			this.el.innerHTML = value;
+		}
+	});
+
+	var mydefine = new Vue({
+		el:"#mydefine",
+		data:{
+			msg:'hello',
+			text:'hhh'
+		}
+	});
+
 });
 
 function mm(){
