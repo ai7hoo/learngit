42b80aa859199caa72e4ee4f0cfd8875f1215da1
renqun <renqun@bandenghui.com>
2016-05-11 18:26:09 +0800

    no message

---
+++ 0509/main.js
@@ -0,0 +1,19 @@
+var DocumentRow = Backbone.View.extend({
+
+  tagName: "div",
+
+  className: "box",
+
+  events: {
+    "click .icon":          "open",
+    "click .button.edit":   "openEditDialog",
+    "click .button.delete": "destroy"
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+
+  render: function() {}
+
+});
